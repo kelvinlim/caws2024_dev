29a13858eda493c768e97653eac0b93c8b388525
Kelvin O. Lim <kolim@umn.edu>
2024-05-22 17:32:25 -0500

    update

--- run_tetrad.sh
+++ run_tetrad.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # run tetrad on mac or windows
 
-java -Dsun.java2d.xrender=false -Dsun.java2d.pmoffscreen=false -jar /caws2024/bin/tetrad-gui-7.6.4-launch.jar
+java -Dsun.java2d.xrender=false -Dsun.java2d.pmoffscreen=false -jar /caws2024/bin/tetrad-gui-7.6.4-launch.jar &
+
+exit 0
+
 
